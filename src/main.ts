3fc288747bbba4b34340869ddc69a9d9588694e1
WiktorB2004 - Ubuntu <byrka.w@gmail.com>
2024-01-14 15:25:50 +0100

    Add TODO(#1)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+// TODO(#1): Create database module
+// TODO: Implement authentication/authorization
+// TODO: Test and validate the complete REST API
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
